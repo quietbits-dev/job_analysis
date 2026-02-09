6542b44149408e2f400d54f2221418f381df92a6
Ethen <rhbke@outlook.com>
2026-02-09 23:40:32 +0800

    add more pages

--- main.go
+++ main.go
@@ -22,37 +22,37 @@ func main() {
 		return
 	}
 
-	for i := 1; i <= 5; i++ { //並發爬取 5 頁
+	for i := 1; i <= 10; i++ { //並發爬取 15 頁
 		wg.Add(1)
 		go func(index int) {
 			defer wg.Done()
+			for k := index; i <= 5; k++ {
+				pageResults := parser.Gethtml(Link, k)
 
-			pageResults := parser.Gethtml(Link, index)
+				// 將爬取到的每個職缺資訊轉換成 JSON 並上傳
+				for _, job := range pageResults {
+					//fmt.Printf("%s", pageResults[0])
+					// 將整個 job 結構轉換為 JSON 格式，方便未來擴充與使用
+					jsonData, err := json.MarshalIndent(job, "", "  ")
+					if err != nil {
+						fmt.Printf("無法將 job %s 轉換為 JSON: %v\n", job.Title, err)
+						continue
+					}
 
-			// 將爬取到的每個職缺資訊轉換成 JSON 並上傳
-			for _, job := range pageResults {
-				//fmt.Printf("%s", pageResults[0])
-				// 將整個 job 結構轉換為 JSON 格式，方便未來擴充與使用
-				jsonData, err := json.MarshalIndent(job, "", "  ")
-				if err != nil {
-					fmt.Printf("無法將 job %s 轉換為 JSON: %v\n", job.Title, err)
-					continue
-				}
+					// 使用 MD5 將職缺連結轉換為唯一的 ID 作為檔名
+					hash := md5.Sum([]byte(job.Link)) // 計算 MD5 hash，回傳 [16]byte 陣列
+					jobID := fmt.Sprintf("%x", hash)  // 將 byte 陣列格式化為十六進位字串
 
-				// 使用 MD5 將職缺連結轉換為唯一的 ID 作為檔名
-				hash := md5.Sum([]byte(job.Link)) // 計算 MD5 hash，回傳 [16]byte 陣列
-				jobID := fmt.Sprintf("%x", hash)  // 將 byte 陣列格式化為十六進位字串
+					objectName := fmt.Sprintf("%s.json", jobID)
+					data := bytes.NewReader(jsonData)
 
-				objectName := fmt.Sprintf("%s.json", jobID)
-				data := bytes.NewReader(jsonData)
-
-				if err := repo.GcsUpload(Bucket, objectName, data); err != nil {
-					fmt.Printf("上傳 %s 失敗: %v\n", objectName, err)
+					if err := repo.GcsUpload(Bucket, objectName, data); err != nil {
+						fmt.Printf("上傳 %s 失敗: %v\n", objectName, err)
+					}
 				}
 			}
 		}(i)
 	}
 	wg.Wait()
 	fmt.Println("所有頁面爬取完成")
-
 }
